e02635e827b09c742846490bd959dfdc1850053b
Colin Jaffe <balloonasaurus@gmail.com>
2022-02-03 19:20:21 -0500

    Initial commit.

---
+++ controllers/bookmarkController.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bookmarks = express.Router();
+const {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+  deleteBookmark,
+  updateBookmark,
+} = require("../queries/bookmarks");
+const { checkName, checkBoolean } = require("../validations/checkBookmarks");
+
+// INDEX
+bookmarks.get("/", async (req, res) => {
+  const allBookmarks = await getAllBookmarks();
+  if (allBookmarks[0]) {
+    res.status(200).json(allBookmarks);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+// SHOW
+bookmarks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const bookmark = await getBookmark(id);
+  if (bookmark) {
+    res.json(bookmark);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+
+bookmarks.post("/", checkBoolean, checkName, async (req, res) => {
+  try {
+    const bookmark = await createBookmark(req.body);
+    res.json(bookmark);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+// DELETE
+bookmarks.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedBookmark = await deleteBookmark(id);
+  // If there is an error and object is returned which is truthy. Therefore we used .id to check that the deletedBookmark variable holds the keys and volume we want.
+
+  // since we used the "one" sql method, which returns an object, if the call to the database is successful we can use dot notation in our conditional for error handling
+  // If we used dot "any", which returns multiple rows of data in an array, when the call to the database is successful we CANNOT use dot notation
+  if (deletedBookmark.id) {
+    res.status(200).json(deletedBookmark);
+  } else {
+    res.status(404).json("Bookmark not found");
+  }
+});
+
+// UPDATE
+bookmarks.put("/:id", checkBoolean, checkName, async (req, res) => {
+  const updatedBookmark = await updateBookmark(req.params.id, req.body);
+  // If there is an error and object is returned which is truthy. Therefore we used .id to check that the updatedBookmark variable holds the keys and volume we want.
+
+  // since we used the "one" sql method, which returns an object, if the call to the database is successful we can use dot notation in our conditional for error handling
+  if (updatedBookmark.id) {
+    res.status(200).json(updatedBookmark);
+  } else {
+    res.status(404).json("Bookmark does not exist");
+  }
+});
+
+module.exports = bookmarks;
---
+++ queries/bookmarks.js
@@ -0,0 +1,75 @@
+// const bookmarks = require("../controllers/bookmarkController");
+const db = require("../db/dbConfig");
+
+const getAllBookmarks = async () => {
+  try {
+    const allBookmarks = await db.any("SELECT * FROM bookmarks");
+    // console.log(allBookmarks);
+    return allBookmarks;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getBookmark = async (id) => {
+  try {
+    const oneBookmark = await db.one("SELECT * FROM bookmarks WHERE id=$1", id);
+    return oneBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createBookmark = async (bookmark) => {
+  try {
+    const newBookmark = await db.one(
+      "INSERT INTO bookmarks (name, url, category, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
+      [bookmark.name, bookmark.url, bookmark.category, bookmark.is_favorite]
+    );
+    // console.log(newBookmark);
+    return newBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteBookmark = async (id) => {
+  try {
+    // Use SQL Method one because we only want to delete the one bookmark. If there is an error the request will not resolve
+    // If we only have one index variable we do not need an array
+    const deletedBookmark = await db.one(
+      "DELETE FROM bookmarks WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateBookmark = async (id, bookmark) => {
+  try {
+    const { name, url, category, is_favorite } = bookmark;
+    // Use SQL Method one because we only want to update the one bookmark. If there is an error the request will not resolve
+    // Use an array for the second argument because there are multiple index variables
+    // With PG-Promise the index starts at $1
+    // The variables in the array must correspond to the index you stated in the query.
+    const changedBookmark = await db.one(
+      "UPDATE bookmarks SET name=$1, URL=$2, category=$3, is_favorite=$4 WHERE id=$5 RETURNING *",
+      [name, url, category, is_favorite, id]
+    );
+    return changedBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+// Documentation https://www.npmjs.com/package/pg-promise#documentation
+
+module.exports = {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+  deleteBookmark,
+  updateBookmark,
+};
